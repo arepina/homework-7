b80f536caef9432b94f71f5594ce67603ad4c049
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2019-02-08 15:28:45 +0300

    hw7 fixed

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-var check = true;
-
 function throughOnce(fn) {
   return fn.bind(this);
 }
 
-var logThroughOnce = throughOnce(function(text) {
-  if(check) { console.log(text); }
-  check = !check;
+var logThroughOnce = throughOnce(function(text, callNumber) {
+  if(callNumber % 2 === 0) { console.log(text); }
 });
 
-logThroughOnce("Hello World!"); // "Hello World!"
-logThroughOnce("Hello World!"); //
-logThroughOnce("Hello World!"); // "Hello World!"
-logThroughOnce("Hello World!"); //
+logThroughOnce("Hello World!", 0); // "Hello World!"
+logThroughOnce("Hello World!", 1); //
+logThroughOnce("Hello World!", 2); // "Hello World!"
+logThroughOnce("Hello World!", 3); //
